746fec2909ca3ebaeb286c0dc29adcb2ae1a2d73
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-04-19 02:56:53 -0400

    Updates

---
+++ precache-manifest.5fdfbfeebbd121cc26e91e3e1dc65d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6560f0794a437748c4eb9847bec9d3",
+    "url": "/Movie-React/index.html"
+  },
+  {
+    "revision": "b6d85d488d67474a2a68",
+    "url": "/Movie-React/static/css/main.271f28aa.chunk.css"
+  },
+  {
+    "revision": "a06f2b1218797ab2ff34",
+    "url": "/Movie-React/static/js/2.d05f745b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-React/static/js/2.d05f745b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d85d488d67474a2a68",
+    "url": "/Movie-React/static/js/main.5028dc47.chunk.js"
+  },
+  {
+    "revision": "e5920fe82f7a4ac0a700",
+    "url": "/Movie-React/static/js/runtime-main.ae7d4f78.js"
+  }
+]);
